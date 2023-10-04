f2da5733fc4865ffb14a4427913c4d5944c3daf9
costaa3 <arthurcmcosta@outlook.pt>
2023-10-04 23:08:48 +0100

    show results
    CurrencyConverter now shows the results

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,54 @@ namespace CurrencyConverter
         public MainWindow()
         {
             InitializeComponent();
+            BindingCurrencies();
+        }
+
+
+        public void BindingCurrencies()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Text");
+            dataTable.Columns.Add("Value");
+
+            dataTable.Rows.Add("==Select==", "0");
+            dataTable.Rows.Add("INR", 1);
+            dataTable.Rows.Add("USD", 75);
+            dataTable.Rows.Add("EUR", 85);
+            dataTable.Rows.Add("SAR", 20);
+            dataTable.Rows.Add("POUND", 5);
+            dataTable.Rows.Add("DEM", 43);
+
+            CurrencySelectionComboBox.ItemsSource = dataTable.DefaultView;
+            ToCurrencyComboBox.ItemsSource = dataTable.DefaultView;
+            ToCurrencyComboBox.SelectedValuePath = "Value";
+            ToCurrencyComboBox.DisplayMemberPath = "Text";
+            CurrencySelectionComboBox.SelectedValuePath = "Value";
+            CurrencySelectionComboBox.DisplayMemberPath= "Text";
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!int.TryParse(e.Text ,out int n))
+            {
+                
+                //e.Text = string.Empty; 
+            }
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrencySelectionComboBox.SelectedIndex == 0 || ToCurrencyComboBox.SelectedIndex == 0)
+            {
+                MessageBox.Show(@"Please select the currency");
+                return;
+            }
+            else
+            {
+                double value = double.Parse(UserInputValue.Text);
+                double result= value * (double.Parse(CurrencySelectionComboBox.SelectedValue.ToString())) / double.Parse(ToCurrencyComboBox.SelectedValue.ToString());
+                ResultLabel.Content = result.ToString("N3");
+            }
         }
     }
 }
