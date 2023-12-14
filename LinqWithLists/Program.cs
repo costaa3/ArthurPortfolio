2e3d7285233cad8a3ac8668767b18a02d445e5c3
costaa3 <arthurcmcosta@outlook.pt>
2023-12-14 22:52:06 +0000

    Structure Project

---
+++ LinqWithLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Student
+        {
+
+        }
+
+        public class School
+        {
+
+        }
+    }
+}
